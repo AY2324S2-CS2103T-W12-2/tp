d3bb2a87c6b1a52e1f6ffa3902e74ec9300dea8d
taufiq <taufiqmmhd@gmail.com>
2024-04-03 15:16:26 +0800

    feat: add `FindFreeTimeCommand`
    Adds a new command similar to `FindCommand` except it finds students who are free in the specified timing.

---
+++ src/main/java/seedu/address/logic/commands/FindFreeTimeCommand.java
@@ -0,0 +1,60 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.student.IsFreePredicate;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END_TIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START_TIME;
+
+/**
+ * Finds and lists all students in address book who is free in the specified timings.
+ */
+public class FindFreeTimeCommand extends Command {
+
+    public static final String COMMAND_WORD = "find_free_time";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Finds all students who are free in the time–range specified"
+            + "and displays them as a list with index numbers.\n"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + PREFIX_START_TIME + "0230" + PREFIX_END_TIME + "0330";
+
+    private final IsFreePredicate predicate;
+
+    public FindFreeTimeCommand(IsFreePredicate predicate) {
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredStudentList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_STUDENTS_LISTED_OVERVIEW, model.getFilteredStudentList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof FindFreeTimeCommand)) {
+            return false;
+        }
+
+        FindFreeTimeCommand otherFindCommand = (FindFreeTimeCommand) other;
+        return predicate.equals(otherFindCommand.predicate);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("predicate", predicate)
+                .toString();
+    }
+}
