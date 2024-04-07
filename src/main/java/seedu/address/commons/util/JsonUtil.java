1773b8cd03024d5182b8ce47cf81ac5e43bea456
Yadunand Prem <yadunand@yadunut.com>
2024-04-07 17:30:03 +0800

    Add comments to pass linter

--- src/main/java/seedu/address/commons/util/JsonUtil.java
+++ src/main/java/seedu/address/commons/util/JsonUtil.java
@@ -57,7 +57,7 @@ public class JsonUtil {
      * Returns the JSON object from the given file or {@code Optional.empty()} object if the file is not found.
      * If any values are missing from the file, default values will be used, as long as the file is a valid JSON file.
      *
-     * @param filePath cannot be null.
+     * @param filePath                   cannot be null.
      * @param classOfObjectToDeserialize JSON file has to correspond to the structure in the class given here.
      * @throws DataLoadingException if loading of the JSON file failed.
      */
@@ -86,7 +86,7 @@ public class JsonUtil {
      * Returns the JSON Array object from the given file or {@code Optional.empty()} object if the file is not found.
      * If any values are missing from the file, default values will be used, as long as the file is a valid JSON file.
      *
-     * @param filePath cannot be null.
+     * @param filePath                   cannot be null.
      * @param classOfObjectToDeserialize JSON file has to correspond to the structure in the class given here.
      * @throws DataLoadingException if loading of the JSON file failed.
      */
@@ -111,9 +111,19 @@ public class JsonUtil {
         return Optional.of(jsonArrayFile);
     }
 
-    public static <T> T readJsonString(String jsonString, Class<T> classOfObjectToDeserialize) throws DataLoadingException{
+    /**
+     * Returns the JSON object from the given string.
+     * If any values are missing from the string, default values will be used, * as long
+     * as the string is a valid JSON string.
+     *
+     * @param jsonString    cannot be null.
+     * @param instanceClass JSON string has to correspond to the structure in the class given here.
+     * @return The object deserialized from the JSON string.
+     * @throws DataLoadingException if loading of the JSON string failed.
+     */
+    public static <T> T readJsonString(String jsonString, Class<T> instanceClass) throws DataLoadingException {
         try {
-            return fromJsonString(jsonString, classOfObjectToDeserialize);
+            return fromJsonString(jsonString, instanceClass);
         } catch (IOException e) {
             logger.severe("Error reading from jsonString");
             throw new DataLoadingException(e);
@@ -123,6 +133,7 @@ public class JsonUtil {
     /**
      * Saves the Json object to the specified file.
      * Overwrites existing file if it exists, creates a new file if it doesn't.
+     *
      * @param jsonFile cannot be null
      * @param filePath cannot be null
      * @throws IOException if there was an error during writing to the file
@@ -137,6 +148,7 @@ public class JsonUtil {
 
     /**
      * Converts a given string representation of a JSON data to instance of a class
+     *
      * @param <T> The generic type to create an instance of
      * @return The instance of T with the specified values in the JSON string
      */
@@ -146,8 +158,9 @@ public class JsonUtil {
 
     /**
      * Converts a given instance of a class into its JSON data string representation
+     *
      * @param instance The T object to be converted into the JSON string
-     * @param <T> The generic type to create an instance of
+     * @param <T>      The generic type to create an instance of
      * @return JSON data representation of the given class instance, in string
      */
     public static <T> String toJsonString(T instance) throws JsonProcessingException {
@@ -172,7 +185,6 @@ public class JsonUtil {
          * Gets the logging level that matches loggingLevelString
          * <p>
          * Returns null if there are no matches
-         *
          */
         private Level getLoggingLevel(String loggingLevelString) {
             return Level.parse(loggingLevelString);
--- src/main/java/seedu/address/storage/JsonModuleMapStorage.java
+++ src/main/java/seedu/address/storage/JsonModuleMapStorage.java
@@ -3,9 +3,6 @@ package seedu.address.storage;
 import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 import seedu.address.commons.core.LogsCenter;
