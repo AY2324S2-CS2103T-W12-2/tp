07d3f4fcfab75211841ca11c8cce1ff293c0bc78
taufiq <taufiqmmhd@gmail.com>
2024-04-14 17:28:57 +0800

    feat: Wrap long texts
    Long texts are wrapped so it all fits within the width of the result display so users only need to scroll vertically and not horizontally

--- src/main/java/seedu/address/logic/commands/module/ListModulesCommand.java
+++ src/main/java/seedu/address/logic/commands/module/ListModulesCommand.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE_CODE;
 
 import java.util.List;
 
-import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
